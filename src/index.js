3e6939cd53bd3d74346ce73adc55000a6c90ff28
takashi05160907 <kanaya@a-works.asia>
2023-02-02 20:55:22 +0900

    study-スプレッド構文

--- src/index.js
+++ src/index.js
@@ -100,5 +100,37 @@
 // console.log(message4);
 
 // デフォルト値
-const sayHello = (name = "user") => console.log(`こんにちは！${name}さん`);
-sayHello();
+// const sayHello = (name = "user") => console.log(`こんにちは！${name}さん`);
+// sayHello();
+
+// スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
