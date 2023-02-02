022340f1ef6f1a2bb4d1a5442df6fa2b52a3f2ff
takashi05160907 <kanaya@a-works.asia>
2023-02-02 17:22:05 +0900

    study-DestructuringAssignment

--- src/index.js
+++ src/index.js
@@ -75,5 +75,26 @@
 // const func2 = (str) => str;
 // console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+// 分割代入
+// const myProfile = {
+//   name: "takashi",
+//   age: 33
+// };
+
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// // console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+const myProfile = ["takashi", 33];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}です。`;
+console.log(message4);
